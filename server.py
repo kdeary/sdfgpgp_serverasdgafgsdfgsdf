c1aaf2bee24f73524047ff7b691a7ba2fab4be5b
Korbin Deary <Korbin.Deary45@gmail.com>
2024-04-09 11:09:42 -0400

    ksdjfglksdjlfg

--- server.py
+++ server.py
@@ -29,56 +29,62 @@ users = {
 
 @app.route('/message', methods=['GET'])
 def getMessageRoute():
-	messageId = request.args.get('messageId')
-	print(messageId)
+	try:
+		messageId = request.args.get('messageId')
+		print(messageId)
 
-	if messageId is None:
+		if messageId is None:
+			return 'Invalid Request', '400 Bad Request'
+
+		if not messageId in messages:
+			return 'Not Found', '404 Not Found'
+
+		print('??', messages[messageId])
+		return jsonify({
+			"messageId": messageId,
+			"encryptedMessage": messages[messageId]
+		}), 200
+	except:
 		return 'Invalid Request', '400 Bad Request'
-
-	if not messageId in messages:
-		return 'Not Found', '404 Not Found'
-
-	print('??', messages[messageId])
-	return jsonify({
-		"messageId": messageId,
-		"encryptedMessage": messages[messageId]
-	}), 200
 
 @app.route('/message', methods=['POST'])
 def createMessageRoute():
-	if not request.is_json:
+	try:
+		if not request.is_json:
+			return 'Invalid Request', '400 Bad Request'
+
+		data = request.json
+
+		print(data)
+		
+		callsign = data.get('callsign')
+		message = data.get('message')
+
+		print(callsign, message)
+
+		if not callsign or not message:
+			return 'Invalid Request', '400 Bad Request'
+
+		if not callsign in users:
+			return 'Not Found', '404 Not Found'
+
+		newID = str(uuid.uuid4())
+
+		enc = gpg.encrypt(message, users[callsign], always_trust=True)
+		messages[newID] = str(enc)
+
+		print(enc.status)
+		print(enc.stderr)
+
+		if not enc.ok:
+			return 'Invalid Request', '400 Bad Request'
+
+		return jsonify({
+			"messageId": newID,
+			"encryptedMessage": messages[newID]
+		}), 200
+	except:
 		return 'Invalid Request', '400 Bad Request'
-
-	data = request.json
-
-	print(data)
-	
-	callsign = data.get('callsign')
-	message = data.get('message')
-
-	print(callsign, message)
-
-	if not callsign or not message:
-		return 'Invalid Request', '400 Bad Request'
-
-	if not callsign in users:
-		return 'Not Found', '404 Not Found'
-
-	newID = str(uuid.uuid4())
-
-	enc = gpg.encrypt(message, users[callsign], always_trust=True)
-	messages[newID] = str(enc)
-
-	print(enc.status)
-	print(enc.stderr)
-
-	if not enc.ok:
-		return 'Invalid Request', '400 Bad Request'
-
-	return jsonify({
-		"messageId": newID,
-		"encryptedMessage": messages[newID]
-	}), 200
 
 if __name__ == "__main__":
 	app.run(debug=True)
